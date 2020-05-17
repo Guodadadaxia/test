a918e221d1d451fe4ff57864eb22b9000b50113c
Guodadadaxia <49034219+Guodadadaxia@users.noreply.github.com>
2020-05-17 10:54:04 +0800

    Update leixiefu111.java

--- src/leixiefu111.java
+++ src/leixiefu111.java
@@ -5,6 +5,6 @@ public class leixiefu111 {
         System.out.println(a);
         a = a.replaceAll("'","\\\\'");
         System.out.println(a);
-        System.out.println(a + "leixiefu111");
+        System.out.println(a + "leixiefu211");
     }
 }
