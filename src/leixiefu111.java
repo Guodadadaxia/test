c6ec80ed0623ab104b27b8b902dddf1b7bb9161a
Guodadadaxia <49034219+Guodadadaxia@users.noreply.github.com>
2020-05-17 11:11:53 +0800

    Update leixiefu111.java

--- src/leixiefu111.java
+++ src/leixiefu111.java
@@ -10,3 +10,16 @@ public class leixiefu111 {
         System.out.println(a + "leixiefu131");
     }
 }
+
+public class leixiefu111 {
+    public static void main(String[] args) {
+
+        String a = "BGI's \"Rea\"l-TIME Flu\"o\"rescent RT-PC2";
+        System.out.println(a);
+        a = a.replaceAll("'","\\\\'");
+        System.out.println(a);
+        System.out.println(a + "leixiefu211");
+        System.out.println(a + "leixiefu111");
+        System.out.println(a + "leixiefu131");
+    }
+}
